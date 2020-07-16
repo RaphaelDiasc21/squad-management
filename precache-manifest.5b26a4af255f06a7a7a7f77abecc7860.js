460dd027de26f3d7e29ca5306e113315de5471bb
Raphael Dias <raphaeldiasc16@gmail.com>
2020-07-16 11:57:38 -0300

    Updates

---
+++ precache-manifest.5b26a4af255f06a7a7a7f77abecc7860.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8c91b5a9624b7948a2fc736a6d33a6f",
+    "url": "/squad-management/index.html"
+  },
+  {
+    "revision": "081252c3bf84f8bfdf49",
+    "url": "/squad-management/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d42ca2190f1ae5f73580",
+    "url": "/squad-management/static/css/main.213aee66.chunk.css"
+  },
+  {
+    "revision": "081252c3bf84f8bfdf49",
+    "url": "/squad-management/static/js/2.3ad3d1d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/squad-management/static/js/2.3ad3d1d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d42ca2190f1ae5f73580",
+    "url": "/squad-management/static/js/main.3315533b.chunk.js"
+  },
+  {
+    "revision": "1df83539aa3aa6949025",
+    "url": "/squad-management/static/js/runtime-main.15b80fce.js"
+  }
+]);
